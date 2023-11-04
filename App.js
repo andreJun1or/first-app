43cc6eb5db0d41dd192762629d70311f90ebec46
andreJun1or <andregamer635@gmail.com>
2023-11-04 19:07:20 -0300

    publishing

--- App.js
+++ App.js
@@ -1,10 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+import Teste from './Teste';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={{
+        fontSize: 30
+      }}>Cuida macho</Text>
+      <Text>Digite algo: </Text>
+      <Teste style={styles.input} placeholder='Digite algo' />
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +21,14 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 30
   },
+  input: {
+    borderWidth: 1,
+    borderColor: 'thistle',
+    borderRadius: 5,
+    marginTop: 3,
+    padding: 5,
+    width: '100%'
+  }
 });
---
+++ Teste.js
@@ -0,0 +1,9 @@
+import { TextInput, } from "react-native";
+
+function Teste({ style, placeholder }) {
+  return(
+    <TextInput style={style} placeholder={placeholder}></TextInput>
+  );
+}
+
+export default Teste;
